1aad3a42b789181a6df57e36eae909b562bb7106
skynet1881 <skynettech1992@gmail.com>
2026-04-06 15:56:53 +0200

    add defaults

--- src/udemy_bot/models.py
+++ src/udemy_bot/models.py
@@ -21,11 +21,13 @@ class CourseConfig:
             course_id=data["course_id"],
             course_label=data["course_label"],
             udemy_promotions_url=data["udemy_promotions_url"],
-            coupon_prefix=data["coupon_prefix"],
-            coupon_price=str(data["coupon_price"]),
+            linkedin_message_template=data["linkedin_message_template"],
+
+            coupon_prefix=data.get("coupon_prefix", "DISC"),
+            coupon_price=str(data.get("coupon_price", 9.99)),
             monthly_coupon_limit=int(data.get("monthly_coupon_limit", 3)),
             max_redemptions=int(data.get("max_redemptions", 1000)),
-            linkedin_message_template=data["linkedin_message_template"],
+            
         )
 
 
